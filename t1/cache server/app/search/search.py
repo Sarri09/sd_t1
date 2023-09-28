cc4294c99e90b336fe415504e2c83583426436ce
Andy <72534502+Sarri09@users.noreply.github.com>
2023-09-28 15:06:13 -0300

    changes

--- t1/cache server/app/search/search.py
+++ t1/cache server/app/search/search.py
@@ -11,12 +11,13 @@ class CacheClient:
         self.channel = grpc.insecure_channel(f"{host}:{port}")
         self.stub = cache_service_pb2_grpc.CacheServiceStub(self.channel)
 
-    def get(self, key, simulated=False):
+    def get(self, key, cache_enable, simulated=False):
         start_time = time.time()  # Inicio del temporizador
-
-        response = self.stub.Get(cache_service_pb2.Key(key=key))
         
-        if response.value:
+    
+        response = self.stub.Get(cache_service_pb2.Key(key=key))
+
+        if response.value and cache_enable:
             elapsed_time = time.time() - start_time  # Calcula el tiempo transcurrido
             print(f"Time taken (cache): {elapsed_time:.5f} seconds")
             return response.value
@@ -34,8 +35,9 @@ class CacheClient:
             if value:
                 print("Key found in JSON. Adding to cache...")
                 
-                # Agregando la llave-valor al caché
-                self.stub.Put(cache_service_pb2.CacheItem(key=key, value=value))
+                if cache_enable:
+                    # Agregando la llave-valor al caché
+                    self.stub.Put(cache_service_pb2.CacheItem(key=key, value=value))
                 
                 elapsed_time = time.time() - start_time  # Calcula el tiempo transcurrido
                 if simulated:
@@ -50,7 +52,7 @@ class CacheClient:
                 print("Key not found.")
                 return None
             
-    def simulate_searches(self, n_searches=100):
+    def simulate_searches(self,n_searches, cache_enable):
         keys_to_search = [f"{i}" for i in np.random.randint(1, 101, n_searches)]
 
         # Métricas
@@ -66,7 +68,7 @@ class CacheClient:
             print(f"Searching : {count}/{n_searches}")
             start_time = time.time()
             time_without_cache += 3 + 0.001  # Estimado de tiempo de búsqueda en JSON
-            self.get(key)
+            self.get(key,cache_enable)
             elapsed_time = time.time() - start_time
             time_with_cache += elapsed_time
 
@@ -76,8 +78,21 @@ class CacheClient:
         time_saved = time_without_cache - time_with_cache
         print(f"\nTime saved thanks to cache: {time_saved:.2f} seconds")
         print(f"Number of times JSON lookup was avoided: {avoided_json_lookups}")
-        
 
+    def remove(self, key):
+        try:
+            response = self.stub.Remove(cache_service_pb2.Key(key=key))
+            if response.success:
+                print(f"Key '{key}' removed from cache.")
+            else:
+                print(f"Failed to remove key '{key}' from cache.")
+        except grpc.RpcError as e:
+            print(f"Error removing key '{key}' from cache: {e.details()}")
+
+    def remove_all(self, keys_to_remove):
+        for key in keys_to_remove:
+            self.remove(key)
+            
 if __name__ == '__main__':
 
     client = CacheClient()
@@ -86,19 +101,25 @@ if __name__ == '__main__':
         print("\nChoose an operation:")
         print("1. Get")
         print("2. Simulate Searches")
-        print("3. Exit")
+        print("3. Clear Cache")
+        print("4. Exit")
 
         choice = input("Enter your choice: ")
 
         if choice == "1":
             key = input("Enter key: ")
-            value = client.get(key)
+            value = client.get(key,1)
             if value is not None:
                 print(f"Value: {value}")
         elif choice == "2":
             n_searches = int(input("Enter the number of searches you want to simulate: "))
-            client.simulate_searches(n_searches)
+            cache_enable = bool(input("Do you want to ignore the cache? Enter 0 if you agree, 1 if you are not agree: "))
+            print(cache_enable)
+            client.simulate_searches(n_searches, cache_enable)
         elif choice == "3":
+            keys_to_remove = input("Enter keys to remove from cache (comma-separated): ").split(',')
+            client.remove_all(keys_to_remove)
+        elif choice == "4":
             print("Goodbye!")
             break
         else:
